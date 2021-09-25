1e12918ea180a77c7c3cb64ae44736e1e4609746
kawan97 <pshtiwankawan@gmail.com>
2021-09-25 08:17:04 -0700

    changing nav color

--- dist/components/nav.php
+++ dist/components/nav.php
@@ -5,9 +5,9 @@
                 <div>
                     <!-- Website Logo -->
                     <a href="#" class="flex items-center py-4 px-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-</svg>                        <span class="font-semibold text-gray-500 text-lg">Navigation</span>
+</svg>                        <span class="font-semibold text-green-500 text-lg">Navigation</span>
                     </a>
                 </div>
                 <!-- Primary Navbar items -->

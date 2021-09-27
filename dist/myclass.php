cc2ba8fcc42e413d6aef0263b7a41372e6772eed
kawan97 <pshtiwankawan@gmail.com>
2021-09-27 04:54:05 -0700

    add myclass to user without layout

--- dist/myclass.php
+++ dist/myclass.php
@@ -12,6 +12,11 @@ if(isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSI
     include './dbcon.php';
 
     //select for user
+    $sql="select enrollstu.id,enrollstu.studentname,enrollstu.status,class.classname From enrollstu 
+    INNER JOIN class ON enrollstu.classid=class.is;
+    WHERE enrollstu.studentname=?;"; 
+    $stmt=$pdo->prepare($sql); 
+    $stmt->execute(array($_SESSION['username']));
 
 
     //select  for teacher
@@ -48,27 +53,33 @@ if(isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSI
   <h1 class="text-center text-black text-2xl	">My Class</h1>
 <?php
  while ($row = $stmt->fetch()) { 
-     if($row['status'] == 'deactivate'){
-         $link='';
-        $bg_color='bg-red-100';
-     }else{
-        $bg_color='bg-green-100';
-        $link='<a  href="class?id='.$row['id'].'" class="flex-shrink-0 border-transparent border-4 bg-green-500 mx-4 text-white hover:text-green-800 text-sm py-1 px-2 rounded" type="button">
-        Manage   
-           </a>';
+  if($_SESSION['role'] == 'teacher'){
+    if($row['status'] == 'deactivate'){
+        $link='';
+       $bg_color='bg-red-100';
+    }else{
+       $bg_color='bg-green-100';
+       $link='<a  href="class?id='.$row['id'].'" class="flex-shrink-0 border-transparent border-4 bg-green-500 mx-4 text-white hover:text-green-800 text-sm py-1 px-2 rounded" type="button">
+       Manage   
+          </a>';
 
-     }
-  echo '
-  <div class="w-full mx-auto max-w-sm '.$bg_color.'  my-3 rounded-xl">
-  <div class="flex items-center border-b rounded-xl border-gray-500 py-2">
+    }
+ echo '
+ <div class="w-full mx-auto max-w-sm '.$bg_color.'  my-3 rounded-xl">
+ <div class="flex items-center border-b rounded-xl border-gray-500 py-2">
 <span class="w-full text-gray-700 mx-3 py-1 px-2 leading-tight">name: '.$row['classname'].', teacher:'.$row['teachername'].' ,status : '.$row['status'].'</span>  
 '.$link.'
-  </div>
+ </div>
 
 </div>
-  
-  
-  ';
+ ';
+      
+}
+
+  if($_SESSION['role'] == 'student'){
+      print_r($row);
+
+  }
 
 }
 ?>
